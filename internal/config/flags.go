c3126779caefd61488a0f7e89a322b81c6881b80
Ruslan Kizilov <rakizilov@avito.ru>
2024-12-30 16:47:59 +0300

    iter4: fix flags

--- internal/config/flags.go
+++ internal/config/flags.go
@@ -1,7 +1,10 @@
 package config
 
 import (
+	"errors"
 	"flag"
+	"strconv"
+	"strings"
 )
 
 var (
@@ -9,8 +12,41 @@ var (
 	FlagShortURL string
 )
 
+type NetAddress struct {
+	Host string
+	Port int
+}
+
 func ParseFlags() {
 	flag.StringVar(&FlagRunAddr, "a", ":8080", "address and port to run server")
-	flag.StringVar(&FlagShortURL, "b", "http://localhost:8080/", "short url address")
+
+	addr := new(NetAddress)
+	_ = flag.Value(addr)
+	flag.Var(addr, "b", "Net address host:port")
+
 	flag.Parse()
+
+	if addr.Host == "" {
+		FlagShortURL = "http://localhost:8080/"
+	} else {
+		FlagShortURL = addr.String()
+	}
+}
+
+func (a NetAddress) String() string {
+	return a.Host + ":" + strconv.Itoa(a.Port) + "/"
+}
+
+func (a *NetAddress) Set(s string) error {
+	hp := strings.Split(s, ":")
+	if len(hp) != 3 {
+		return errors.New("Need address in a form host:port")
+	}
+	port, err := strconv.Atoi(hp[2])
+	if err != nil {
+		return err
+	}
+	a.Host = hp[0] + hp[1]
+	a.Port = port
+	return nil
 }
