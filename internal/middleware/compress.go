e48982069e48440e654780e4cc2c924398cb2b16
Ruslan Kizilov <rakizilov@avito.ru>
2025-01-25 13:10:05 +0300

    iter8

--- internal/middleware/compress.go
+++ internal/middleware/compress.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"bytes"
 	"compress/gzip"
 	"strings"
 
@@ -20,22 +21,36 @@ func Gzip() gin.HandlerFunc {
 			return
 		}
 
-		//var b bytes.Buffer
-		//gz := gzip.NewWriter(&b)
-		//defer gz.Close()
-		//
-		//writer := &responseWriterGzip{ResponseWriter: c.Writer, gzipWriter: gz}
-		//c.Writer = writer
-		//
+		var b bytes.Buffer
+		gz := gzip.NewWriter(&b)
+		defer gz.Close()
+
+		writer := &responseWriterGzip{ResponseWriter: c.Writer, gzipWriter: gz}
+		c.Writer = writer
+
 		c.Next()
-		//
-		//if err := gz.Close(); err != nil {
-		//	c.Error(err)
-		//	return
-		//}
-		//
-		//c.Header("Content-Encoding", "gzip")
-		//c.Header("Content-Type", writer.Header().Get("Content-Type"))
+
+		if err := gz.Close(); err != nil {
+			c.Error(err)
+			return
+		}
+
+		c.Header("Content-Encoding", "gzip")
+		c.Header("Content-Type", writer.Header().Get("Content-Type"))
 		//c.Writer.Write(b.Bytes())
+
+		c.Writer.WriteHeader(writer.status)
+		c.Writer.Write(b.Bytes())
 	}
+}
+
+// WriteHeader captures the status code.
+func (w *responseWriterGzip) WriteHeader(status int) {
+	w.status = status
+	w.ResponseWriter.WriteHeader(status)
+}
+
+// Write writes the response body to the gzip writer.
+func (w *responseWriterGzip) Write(data []byte) (int, error) {
+	return w.gzipWriter.Write(data)
 }
