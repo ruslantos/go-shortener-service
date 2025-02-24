3ff062571cc809abfb0710b95d66c58485cf5afd
Ruslan Kizilov <rakizilov@avito.ru>
2025-02-24 20:54:54 +0300

    iter15

--- internal/middleware/logger/logger.go
+++ internal/middleware/logger/logger.go
@@ -67,13 +67,13 @@ func LoggerChi(logger *zap.Logger) func(next http.Handler) http.Handler {
 			//	cookies = append(cookies, auth)
 			//}
 
-			//logger.Info("Outgoing response",
-			//	zap.Int("status", rw.Status()),
-			//	zap.Duration("duration", duration),
-			//	zap.Int("response_size", rw.Size()),
-			//	zap.String("response_body", rw.body.String()),
-			//	zap.Strings("cookies", cookies),
-			//)
+			logger.Info("Outgoing response",
+				zap.Int("status", rw.Status()),
+				//zap.Duration("duration", duration),
+				//zap.Int("response_size", rw.Size()),
+				//zap.String("response_body", rw.body.String()),
+				//zap.Strings("cookies", cookies),
+			)
 		})
 	}
 }
