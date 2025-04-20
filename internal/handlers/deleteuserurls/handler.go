99bb174082b03f871f69e1c1b77e893401f69420
Ruslan Kizilov <rakizilov@avito.ru>
2025-04-20 18:21:07 +0300

    iter18

--- internal/handlers/deleteuserurls/handler.go
+++ internal/handlers/deleteuserurls/handler.go
@@ -11,21 +11,25 @@ import (
 	"github.com/ruslantos/go-shortener-service/internal/service"
 )
 
+// linkService интерфейс для сервиса, который обрабатывает удаление URL.
 type linkService interface {
 	ConsumeDeleteURLs(data service.DeletedURLs)
 }
 
+// Handler обработчик для удаления пользовательских URL.
 type Handler struct {
 	service linkService
 }
 
+// DeleteUserURLsRequest структура запроса на удаление пользовательских URL.
 type DeleteUserURLsRequest []string
 
+// New создаёт новый обработчик для удаления пользовательских URL.
 func New(service linkService) *Handler {
 	return &Handler{service: service}
 }
 
-// Handle Обработка запроса на удаление пользовательских URL
+// Handle обрабатывает запрос на удаление пользовательских URL.
 func (h *Handler) Handle(w http.ResponseWriter, r *http.Request) {
 	bodyRaw, err := io.ReadAll(r.Body)
 	if err != nil {
@@ -62,6 +66,7 @@ func (h *Handler) Handle(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusAccepted)
 }
 
+// getUserIDFromContext извлекает идентификатор пользователя из контекста запроса.
 func getUserIDFromContext(ctx context.Context) (string, error) {
 	userID, ok := ctx.Value(auth.UserIDKey).(string)
 	if !ok {
