a69abd98967e35c26127c8473f14426986829d27
Ruslan Kizilov <rakizilov@avito.ru>
2025-05-02 09:39:34 +0300

    iter19

---
+++ cmd/staticlint/multichecker.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/multichecker"
+	"golang.org/x/tools/go/analysis/passes/printf"
+	"golang.org/x/tools/go/analysis/passes/shadow"
+	"golang.org/x/tools/go/analysis/passes/structtag"
+	"honnef.co/go/tools/staticcheck"
+)
+
+func main() {
+	checks := map[string]bool{
+		"SA5000":   true,
+		"SA6000":   true,
+		"SA9004":   true,
+		"fakejson": true,
+	}
+	var mychecks []*analysis.Analyzer
+	for _, v := range staticcheck.Analyzers {
+		if checks[v.Analyzer.Name] {
+			mychecks = append(mychecks, v.Analyzer)
+		}
+	}
+
+	multichecker.Main(
+		printf.Analyzer,
+		shadow.Analyzer,
+		structtag.Analyzer,
+	)
+}
